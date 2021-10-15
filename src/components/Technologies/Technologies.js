a703bd4f240196104c0b2569fbfff357a50ea4fc
Ahmad Saleh <dev.ahmadsaleh@gmail.com>
2021-10-15 21:22:06 +0300

    Building Technologies Section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,41 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section>
+    <SectionDivider />
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range a Technologies in the web Development world.
+      From Backend to the Design.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem"/>
+        <ListContainer>
+          <ListTitle>Front-End Development</ListTitle>
+          <ListParagraph>Experience with React.js</ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem"/>
+        <ListContainer>
+          <ListTitle>Back-End Development</ListTitle>
+          <ListParagraph>Experienced with Building Database, Firebase and Building APIs</ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem"/>
+        <ListContainer>
+          <ListTitle>UI/UX Designing</ListTitle>
+          <ListParagraph>Experience with UX Building, Analysing and UI Designing </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
