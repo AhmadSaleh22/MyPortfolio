d0e7f742dba7205acba76ba46c5488b61ee14ade
Ahmad Saleh <dev.ahmadsaleh@gmail.com>
2021-10-15 19:04:35 +0300

    Building Hero Section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Ahmed Saleh. <br/>I Build things for the web.
+      </SectionTitle>
+      <SectionText>
+        I'm a Software Engineer, 
+        I have 2 years experience leading all phases of the Design and Development process on responsive web Applications.
+      </SectionText>
+      <Button onClick={()=>
+        window.location = 'https://google.com'
+      }>Contact Me</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
